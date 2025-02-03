b5d25c93c12d78a50ad43639f2b9c195121c4852
Jackson Wakefield <jacksonwakefield01@gmail.com>
2025-02-03 03:37:02 -0700

    Deployed Build (still has REST stuff in it, but will remove)

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const isProd = process.env.NODE_ENV === 'production';
+
+const nextConfig = {
+  assetPrefix: isProd ? '/Full-Stack-Pizza-App/' : '',
+  basePath: isProd ? '/Full-Stack-Pizza-App' : '',
+};
 
 export default nextConfig;
