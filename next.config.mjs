3678a8a79725f5e9c886ec8c41c3d311f1396a3b
Jackson Wakefield <jacksonwakefield01@gmail.com>
2025-02-03 13:27:08 -0700

    changing to vercel 2

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@
 const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig = {
-    assetPrefix: process.env.NODE_ENV === 'production' ? '/full-stack-pizza-app/' : '',
+    
 };
 
 export default nextConfig;
