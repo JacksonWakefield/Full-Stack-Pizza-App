69e0f6664f5773ab6c2d1e1100e207deefd8141e
Jackson Wakefield <jacksonwakefield01@gmail.com>
2025-02-03 13:35:52 -0700

    changing to vercel 3

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === 'production';
-
 const nextConfig = {
-    
-};
-
+    experimental: {
+      appDir: true,
+    },
+  };
+  
 export default nextConfig;
