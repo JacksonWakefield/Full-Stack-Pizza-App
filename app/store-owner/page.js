4cc2d4e06a9648b075f0bf59dbde6af1688a4aa3
Jackson Wakefield <jacksonwakefield01@gmail.com>
2025-02-03 18:19:42 -0700

    changing to vercel 8

--- app/store-owner/page.js
+++ app/store-owner/page.js
@@ -18,10 +18,18 @@ export default function StoreOwner() {
     useEffect(() => {
         async function fetchToppings() {
             try {
-                const res = await fetch('https://f0ac-52-53-194-129.ngrok-free.app/toppings/');
+                const res = await fetch('https://f0ac-52-53-194-129.ngrok-free.app/toppings/', {
+                    headers: new Headers({
+                        "ngrok-skip-browser-warning": "69420",
+                        "Access-Control-Allow-Origin": "*", // Allow all origins, you can restrict this to your domain
+                        "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE", // Allow specific HTTP methods
+                        "Access-Control-Allow-Headers": "Content-Type, Authorization", // Allow specific headers
+                    })
+                });
                 if (!res.ok) {
                     throw new Error('Failed to fetch toppings');
                 }
+                console.log(await res.text());
                 const data = await res.json();
                 setToppings(data);
             } catch (error) {
